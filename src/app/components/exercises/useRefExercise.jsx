1b9e95cedb774036651279f77669711dd0bdeef6
MichaelD <enee.kors@mail.ru>
2022-01-30 14:05:00 +0300

    Initial commit

---
+++ src/app/components/exercises/useRefExercise.jsx
@@ -0,0 +1,42 @@
+import React, { useRef } from "react";
+import CollapseWrapper from "../common/collapse";
+
+const UseRefExercise = () => {
+    const divRef = useRef();
+
+    const handleClick = () => {
+        console.log("style2", divRef);
+
+        divRef.current.style.width = "80px";
+        divRef.current.style.height = "150px";
+    };
+    return (
+
+        <CollapseWrapper title="Упражнение">
+            <div ref={divRef}>
+            <p className="mt-3">
+                У вас есть блок, у которого заданы ширина и высота. Добавьте
+                кнопку, при нажатии которой изменятся следующие свойства:
+            </p>
+            <ul>
+                <li>Изменится содержимое блока на &quot;text&quot;</li>
+                <li>высота и ширина станут равны 150 и 80 соответственно</li>
+            </ul>
+        </div>
+            <div
+                className="bg-primary d-flex flex-row justify-content-center align-items-center rounded"
+                style={{
+                    height: 40,
+                    width: 60,
+                    color: "white"
+                }}
+            >
+                <small onClick={handleClick}>
+                    Блок
+                </small>
+            </div>
+        </CollapseWrapper>
+    );
+};
+
+export default UseRefExercise;
